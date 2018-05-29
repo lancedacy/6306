fd9187c9d0d8b55c0c7c455575675aa3c994ba8c
Lance Dacy <lance@big-agile.com>
2018-05-29 07:38:37 -0400

    updating scratch

---
+++ Admin/Quizzes.r
@@ -0,0 +1,3 @@
+library(swirl)
+install_course_github('johnsanterre','All_The_Quizzes')
+swirl()
--- Units/04/Unit04Project/Scratch04.R
+++ Units/04/Unit04Project/Scratch04.R
@@ -1,34 +1,58 @@
-#Question 1
+# 1a.Install the library
 library("fivethirtyeight")
 fileURL <- "https://raw.githubusercontent.com/rudeboybert/fivethirtyeight/master/data-raw/classic-rock/classic-rock-raw-data.csv"
+
+# 1b. In the listing of Data sets in package ‘fivethirtyeight,’ assign the 
+# eighteenth data set to an object ‘df.’
 library(repmis)
 df <-repmis::source_data(fileURL)
+
+# 1c. Use a more detailed list of the data sets to 
+# write out the URL in a comment to the related news story.
 newsstory <- "http://fivethirtyeight.com/features/why-classic-rock-isnt-what-it-used-to-be/"
-colnames(df)
+
+#1d. Using R command(s), give the dimensions and column names of this data frame.
 dim(df)
+colnames(df)
 
 
-#Question 2
+# 2a. Write an R command that gives you the column names of the data frame. 
+# Right after that, write one that counts the number of columns but not rows. 
+# Hint: The number should match one of your numbers in Question 1d for dimensions.
+colnames(df)
 ncol(df)
-unique(df$COMBINED)
 
-head(df)
-
-
-#Building the table
-unique(df$COMBINED)
-
-aggregate(data.frame(count = major_count), list(value = v), length)
-
-#if you have multiple factors try this
+# 2b. Generate a count of each unique major_category in the data frame. 
+# I recommend using libraries to help. I have demonstrated one briefly 
+# in live-session. To be clear, this should look like a matrix or data 
+# frame containing the major_category and the frequency it occurs in the dataset. 
+# Assign it to major_count.
 
 library("dplyr")
 dfSong_table <- df %>% group_by(df$COMBINED) %>% summarize(major_count=n())
-dfCallSign_table <- df %>% group_by(df$CALLSIGN) %>% summarize(major_count=n())
-dfYear_table <- df %>% group_by(as.POSIXlt(as.numeric(df$TIME),origin="1970-01-01",tz="GMT"))) %>% summarize(major_count=n())
-head(as.POSIXlt(as.numeric(df$TIME),origin="1970-01-01",tz="GMT"))
-head(as.Date(as.POSIXct(df$TIME, origin="1970-01-01")))
+# dfCallSign_table <- df %>% group_by(df$CALLSIGN) %>% summarize(major_count=n())
+# dfYear_table <- df %>% group_by(as.POSIXlt(as.numeric(df$TIME),origin="1970-01-01",tz="GMT"))) %>% summarize(major_count=n())
+# head(as.POSIXlt(as.numeric(df$TIME),origin="1970-01-01",tz="GMT"))
+# head(as.Date(as.POSIXct(df$TIME, origin="1970-01-01")))
 
+# 2c. To make things easier to read, enter par(las=2) before your plot to make the text 
+# perpendicular to the axis. Make a barplot of major_count. Make sure to label the title with 
+# something informative (check the vignette if you need), label the x and y axis, and make it 
+# any color other than grey. Assign the major_category labels to their respective bar. Flip the 
+# barplot horizontally so that bars extend to the right, not upward. All of these options can be 
+# done in a single pass of barplot(). Note: It’s okay if it’s wider than the preview pane.
 
-#write to CSV file with no row names
+# Fitting Labels 
+par(las=2) # make label text perpendicular to axis
+par(mar=c(5,8,4,2)) # increase y-axis margin.
+
+major_counts <- table(dfSong_table$major_count)
+#barplot(major_counts, main="Song Distribution", horiz=TRUE, names.arg=c(dfSong_table$`df$COMBINED`), cex.names=0.8)
+barplot(major_counts, main="Song Distribution", horiz=TRUE, names.arg=c(dfSong_table$`df$COMBINED`), cex.names=0.8)
+
+counts <- table(dfSong_table$major_count)
+barplot(counts, main="Car Distribution", horiz=TRUE,
+        names.arg=c(dfSong_table$`df$COMBINED`))
+
+# 2d. Write the fivethirtyeight data to a csv file. Make sure that it does not have row labels.
 write.csv(df, file = "df.csv", row.names=FALSE)
